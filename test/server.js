a469feb361519b5aa7f887b70e145151f458f733
librah <librah@tw.ibm.com>
2015-11-16 17:57:24 +0800

    Update based on pull request comments

--- test/server.js
+++ test/server.js
@@ -2,15 +2,40 @@
 var createFlow = require('../index.js');
 const express = require('express');
 
-let myCustomParamResolver = function(paramName, paramValue) {
-    let value = paramValue+"-resolved";
-    return value;
+
+//
+// The following function simulates the integration with APIm
+// 
+const cls = require('continuation-local-storage');
+
+function initAPImContext(req, resp, next) {
+    var apimCtx = cls.createNamespace('apim');
+    apimCtx.run(function() {
+	    apimCtx.set("request.verb", req.method);
+	    apimCtx.set("request.path", req.path);
+	    apimCtx.set("target-host", "w3.ibm.com");
+	    apimCtx.set("timeout", 10);
+	    apimCtx.set("username", "test");
+	    apimCtx.set("password", "test");
+	    
+	    next();
+    });
+} 
+
+//
+// APIm's specific parameter resolver callback
+//
+let apimParamResolver = function(context, name, value) {
+    let apimCtx = cls.getNamespace('apim');
+    return value.replace(/\$\(([^)]+)\)/gm, function(m, g1) {
+        return apimCtx.get(g1);
+    });
 };
 
-var flow = createFlow({config: "flow.yaml", paramResolver: myCustomParamResolver});
+var flow = createFlow({config: "flow.yaml", paramResolver: apimParamResolver});
 
 var app = express();
-app.post('/*', flow);
+app.post('/*', [ initAPImContext, flow ]);
 
 var server = app.listen(7777, function() {
     console.log("[server.js] started");
