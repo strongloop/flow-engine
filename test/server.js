38747d67927971a6b73049ba2eada8c44371965b
librah <librah@tw.ibm.com>
2015-11-17 11:45:36 +0800

    Use flow-engine's API to create APIM context

--- test/server.js
+++ test/server.js
@@ -6,27 +6,42 @@ const express = require('express');
 //
 // The following function simulates the integration with APIm
 // 
-const cls = require('continuation-local-storage');
-
 function initAPImContext(req, resp, next) {
-    var apimCtx = cls.createNamespace('apim');
+    // Two options to create an APIm Context
+    // Option1: use 'continuation-local-storage' module, ex:
+    //   const cls = require('continuation-local-storage');
+    //   var apimCtx = cls.createNamespace('apim');
+    //
+    // Option2: use flow-engine's context API
+    //   the flow-engine's context API uses 'continuation-local-storage'
+    //   module as well
+    
+    var contextFactory = require('../index').Context;
+    var apimCtx = contextFactory.getCurrent();
+    if (!apimCtx) {
+	apimCtx = contextFactory.create();
+	apimCtx.bindEmitter(req);
+	apimCtx.bindEmitter(resp);
+    }
     apimCtx.run(function() {
 	    apimCtx.set("request.verb", req.method);
 	    apimCtx.set("request.path", req.path);
-	    apimCtx.set("target-host", "w3.ibm.com");
+	    apimCtx.set("target-host", "localhost:8888");
 	    apimCtx.set("timeout", 10);
 	    apimCtx.set("username", "test");
 	    apimCtx.set("password", "test");
 	    
 	    next();
     });
-} 
+}
 
 //
 // APIm's specific parameter resolver callback
 //
 let apimParamResolver = function(context, name, value) {
-    let apimCtx = cls.getNamespace('apim');
+    // let apimCtx = cls.getNamespace('apim');
+    let apimCtx = require('../index').Context.getCurrent();
+    
     return value.replace(/\$\(([^)]+)\)/gm, function(m, g1) {
         return apimCtx.get(g1);
     });
@@ -41,3 +56,11 @@ var server = app.listen(7777, function() {
     console.log("[server.js] started");
 });
 
+
+/** an echo/loopback service **/
+var loopback = express();
+loopback.all('/*', function(req, resp, next) {
+    let message = "Loopback: " + req.protocol + "://" + req.hostname + req.url + "\n";
+    resp.send(message);
+});
+loopback.listen(8888);
